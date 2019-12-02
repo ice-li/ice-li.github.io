a9900420f63201f6927e18ff2b8fdf2f825ea473
ice-li <ili@theknotww.com>
2019-12-02 12:17:35 +0800

    react

---
+++ precache-manifest.ae646ab253bfa31a0dc28d2d0e093357.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75aaf28268843636a2351f50e753e900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b7a623f904927dc085",
+    "url": "/static/js/2.c323343f.chunk.js"
+  },
+  {
+    "revision": "6c248013f58bb9738610",
+    "url": "/static/js/main.28f790ac.chunk.js"
+  },
+  {
+    "revision": "69a3ea962a00cab465ed",
+    "url": "/static/js/runtime-main.83ea3b8b.js"
+  }
+]);
